e7a3c5645c7f21276a0649e9ecf17e0619f88ef5
saramangelo <saramangelo@gmail.com>
2023-02-03 09:55:42 -0800

    deleted reaction schema, it was a model when I only wanted it to be a schema that is embedded in thought model, added reaction schema above in thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,8 +1,28 @@
 const { Schema, Types } = require('mongoose');
 
+// reaction schema (called below)
 const reactionSchema = new Schema(
-  
-)
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username:  {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // Use a getter method to format the timestamp on query
+    },
+  },
+);
 
 const thoughtSchema = new Schema(
   {
