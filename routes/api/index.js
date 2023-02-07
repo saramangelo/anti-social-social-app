67490f2118b27b85806fb8c7f05e36704e5f8e77
saramangelo <saramangelo@gmail.com>
2023-02-06 19:47:28 -0800

    got all routes working, had to comment out .populate friends and thoughts for get user by id in order for it to work...

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const thoughtRoutes = require('./thoughtRoutes');
 const userRoutes = require('./userRoutes');
-const reactionRoutes = require('./reactionRoutes');
+
 
 
 router.use('/thoughts', thoughtRoutes);
